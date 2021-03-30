3561a0d3a05e8d66ef93802a65281557b31f19ad
Igniteh <n0majo00@students.oamk.fi>
2021-03-30 12:49:03 +0300

    first

---
+++ products/searchproducts.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+try {
+    $db = openDb();
+    selectAsJson($db, 'select * from tuoteryhma');
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
+
+?>
